a7027b9273eca3b8ade4c9dc060b3d8a8e2377a2
Lee SeungEon <dltmddjs98@naver.com>
2026-05-29 17:49:44 +0900

    update

--- workspace/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-    public static void hello() { // static 메소드 (정적 메소드, 클래스 메소드)
-        System.out.println("안녕");
-    }
-    public static void main(String[] args) {
-        Test.hello(); // 메소드 이름으로 호출
-    }
-}
